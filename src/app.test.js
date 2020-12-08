5c8b1b329b10d0ebbdefcddc5fdd963ea30fdb12
Svetlana Arkhipova <sve.tochka@inbox.ru>
2020-12-08 21:31:01 +0300

    initial commit

---
+++ src/app.test.js
@@ -0,0 +1,18 @@
+// import { TestScheduler } from 'jest';
+import byField from './app';
+
+test('comparison', () => {
+  const users = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const result = users.sort(byField('health'));
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(result).toEqual(expected);
+});
